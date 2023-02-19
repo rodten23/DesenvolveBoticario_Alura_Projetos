a443258e80d78838dfb4ab0eb4a2fe208a707178
Rodrigo Alves Tenório <rodten23@yahoo.com.br>
2023-02-19 06:21:37 -0300

    :bug: fix: Validação no arquivo JogosEmPython para aceitar apenas as opções de jogos 1, 2 e 3.

--- JogosEmPython.py
+++ JogosEmPython.py
@@ -9,16 +9,17 @@ print('***** Que a diversão comece! *****\n')
 print('Os jogos disponíveis são:\n')
 print('(1) Adivinhação   (2) Jogo da Velha   (3) Jogo da Forca\n')
 
-jogo = int(input('Qual destes jogos gostaria de iniciar?: '))
+jogo = input('Qual destes jogos gostaria de iniciar?: ')
 
-while(jogo != 1 and jogo != 2 and jogo != 3):
-    print('\nVocê digitou um código de jogo inválido.\nPor favor, digite 1, 2 ou 3 para iniciar o jogo.\n')
-    jogo = int(input('Por favor, digite o número do jogo escolhido: '))
+while (jogo != "1" and jogo != "2" and jogo != "3"):
+    jogo = input('\nVocê digitou um código de jogo inválido.\nPor favor, digite 1, 2 ou 3 para iniciar o jogo: ')
 
-if(jogo == 1):
+escolha = int(jogo)
+
+if(escolha == 1):
     JogoAdivinhacao.jogar()
-elif(jogo == 2):
+elif(escolha == 2):
     JogoDaVelha.jogar()
-elif(jogo == 3):
+elif(escolha == 3):
     JogoForca.jogar()
 
