c3b895ab24dab123423aa3262cfd11b8c709787d
Rodrigo Alves Tenório <rodten23@yahoo.com.br>
2023-02-19 01:28:14 -0300

    :bug: fix: Inclusão da validação de opção de jogo (linhas 14 a 16) do arquido JogosEmPython.

--- JogosEmPython.py
+++ JogosEmPython.py
@@ -11,6 +11,10 @@ print('(1) Adivinhação   (2) Jogo da Velha   (3) Jogo da Forca\n')
 
 jogo = int(input('Qual destes jogos gostaria de iniciar?: '))
 
+while(jogo != 1 and jogo != 2 and jogo != 3):
+    print('\nVocê digitou um código de jogo inválido.\nPor favor, digite 1, 2 ou 3 para iniciar o jogo.\n')
+    jogo = int(input('Por favor, digite o número do jogo escolhido: '))
+
 if(jogo == 1):
     JogoAdivinhacao.jogar()
 elif(jogo == 2):
